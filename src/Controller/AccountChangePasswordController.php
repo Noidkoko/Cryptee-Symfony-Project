78a832b2df1c4885819a185f6c993f1fa7350e27
Noidkoko <marvinguillot@hotmail.fr>
2022-09-01 10:36:52 +0200

    10h36

--- src/Controller/AccountChangePasswordController.php
+++ src/Controller/AccountChangePasswordController.php
@@ -3,20 +3,51 @@
 namespace App\Controller;
 
 use App\Form\ChangePasswordType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccountChangePasswordController extends AbstractController
 {
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/mon-compte/modifier-mon-mot-de-passe', name: 'account_change_password')]
-    public function index(): Response
+    public function index(Request $request, UserPasswordHasherInterface $hasher): Response
     {
+        $notification = null;
+
         $user = $this->getUser();
         $form = $this->createForm(ChangePasswordType::class, $user);
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $old_pwd = $form->get('old_password')->getData();
+            
+            if ($hasher->isPasswordValid($user, $old_pwd)) {
+                $new_pwd = $form->get('new_password')->getData();
+                $password = $hasher->hashPassword($user, $new_pwd);
+                $user->setPassword($password);
+                $this->entityManager->flush();
+                $notification = "Votre mot de passe à bien été modifié";
+            } else {
+                $notification = "Un problème est survenu lors de la modification de votre mot de passe";
+            }
+        }
+
         return $this->render('account/password.html.twig', [
             'form' => $form->createView(),
+            'notification' => $notification
         ]);
     }
 }
--- src/Form/ChangePasswordType.php
+++ src/Form/ChangePasswordType.php
@@ -9,6 +9,9 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\Length;
 
 class ChangePasswordType extends AbstractType
 {
@@ -18,13 +21,45 @@ class ChangePasswordType extends AbstractType
     ): void {
         $builder
             ->add('username', TextType::class, [
-                'disabled' => true
+                'disabled' => true,
+                'label' => "Votre pseudo"
             ])
-            ->add( 'password', PasswordType::class)
             ->add('email', EmailType::class, [
-                'disabled' => true
+                'disabled' => true,
+                'label' => "Votre e-mail"
+            ])
+            ->add( 'old_password', PasswordType::class, [
+                'label' => 'Mot de passe actuel',
+                'mapped' => false,
+                'attr' => [
+                    'placeholder' => "123456789"
+                ]
+            ] )
+            ->add('new_password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'invalid_message' => 'La confirmation doit être identique au mot de passe !',
+                'required' => true,
+                'constraints' => new Length([
+                    'min' => 8,
+                ]),
+                'first_options' => [
+                    'label' => 'Votre nouveau mot de passe',
+                    'attr' => [
+                        'placeholder' => '123456789',
+                    ],
+                ],
+                'second_options' => [
+                    'label' => 'Confirmez votre nouveau mot de passe',
+                    'attr' => [
+                        'placeholder' => '123456789',
+                    ],
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "Modifier mon mot de passe"
             ]);
-    }
+        }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
