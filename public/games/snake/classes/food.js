89d6bfd419ecb8fbf95546bf93dfc42a87ecd446
Noidkoko <marvinguillot@hotmail.fr>
2022-09-05 20:47:15 +0200

    20:47

--- public/games/snake/classes/food.js
+++ public/games/snake/classes/food.js
@@ -11,7 +11,7 @@ class Food {
 
     draw() {
         let food = new Image()
-        food.src = 'Pomme.webp'
+        food.src = './food.png'
         ctx.drawImage(food, this.x * this.size, this.y * this.size, this.size, this.size)
     }
 }
